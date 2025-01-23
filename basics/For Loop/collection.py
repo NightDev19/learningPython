7395b70ca43070f2fab85389d06dea6d285c1cc6
NightDev/ <115393612+NightDev19@users.noreply.github.com>
2025-01-24 00:12:38 +0800

    Starting For Loop In-Depth

---
+++ basics/For Loop/collection.py
@@ -0,0 +1,5 @@
+basket = ["Apple", "Banana", "Cherry"]
+#for every fruit in basket, 
+for fruit in basket:
+    #print the fruit
+    print(fruit)
---
+++ basics/For Loop/else.py
@@ -0,0 +1,6 @@
+basket = ["Apple", "Banana", "Cherry"]
+
+for fruit in basket:
+    print(f"I ate the {fruit}")
+else:
+    print("The basket is empty")
